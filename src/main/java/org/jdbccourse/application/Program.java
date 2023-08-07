5385500905a138bcfb07ef2dea8e32d5431fa3ef
Rodrigo <rodrigodrebes97@gmail.com>
2023-08-07 15:23:49 -0300

    department class, main class

--- src/main/java/org/jdbccourse/application/Program.java
+++ src/main/java/org/jdbccourse/application/Program.java
@@ -1,6 +1,7 @@
 package org.jdbccourse.application;
 
 import org.jdbccourse.db.DB;
+import org.jdbccourse.model.entities.Department;
 
 import java.sql.Connection;
 
@@ -9,5 +10,8 @@ public class Program {
 
         Connection conn = DB.getConnection();
         DB.closeConnection();
+
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
     }
 }

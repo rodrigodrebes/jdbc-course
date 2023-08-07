f28b69b2b2b3d842c21082790e33bb1d9d5f5518
Rodrigo <rodrigodrebes97@gmail.com>
2023-08-07 15:35:15 -0300

    seller class

--- src/main/java/org/jdbccourse/application/Program.java
+++ src/main/java/org/jdbccourse/application/Program.java
@@ -2,8 +2,10 @@ package org.jdbccourse.application;
 
 import org.jdbccourse.db.DB;
 import org.jdbccourse.model.entities.Department;
+import org.jdbccourse.model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
@@ -11,7 +13,11 @@ public class Program {
         Connection conn = DB.getConnection();
         DB.closeConnection();
 
+
+
         Department obj = new Department(1,"Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
